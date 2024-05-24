2a2f9e6f4ff42754cc2628b557379ef3c1d34719
Ludiamen <maxvas1001@gmail.com>
2024-05-24 16:35:09 +0300

    add README.md

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+import Cell from './cell.js';
+import rawPosition from './goblin.js';
+
+const cell = new Cell();
+
+cell.draw();
+
+rawPosition();
+
+
---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+// import { resolve } from 'path';
+// import HtmlWebPackPlugin from 'html-webpack-plugin';
+// import MiniCssExtractPlugin, { loader as _loader } from 'mini-css-extract-plugin';
+
+// export const output = {
+//   path: resolve(__dirname, 'dist'),
+// };
+// export const module = {
+//   rules: [
+//     {
+//       test: /\.js$/,
+//       exclude: /node_modules/,
+//       use: {
+//         loader: 'babel-loader',
+//       },
+//     },
+//     {
+//       test: /\.html$/,
+//       use: [
+//         {
+//           loader: 'html-loader',
+//         },
+//       ],
+//     },
+//     {
+//       test: /\.css$/,
+//       use: [
+//         _loader,
+//         'css-loader',
+//       ],
+//     },
+//   ],
+// };
+// export const plugins = [
+//   new HtmlWebPackPlugin({
+//     template: './src/index.html',
+//     filename: './index.html',
+//   }),
+//   new MiniCssExtractPlugin({
+//     filename: '[name].css',
+//     chunkFilename: '[id].css',
+//   }),
+// ];
+
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader, 'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+};
